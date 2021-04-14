49b1944fad333c2a6f5697484904db10a5bf5ce3
aud203 <ifmo@gmail.com>
2021-04-14 22:05:10 +0300

    collections ++

--- src/com/profi/jjd/lesson13/hw/car/CarComparing.java
+++ src/com/profi/jjd/lesson13/hw/car/CarComparing.java
@@ -1,9 +1,13 @@
 package com.profi.jjd.lesson13.hw.car;
 
+import java.util.Collections;
+
 public class CarComparing {
     public static void main(String[] args) {
 
-        // сортировать по значениям свойств: price, color, brand
+        // сортировать по
+        // price, color, brand
+
         Car blackOpel = new Car("black", "opel", 2000);
         Car redOpel = new Car("red", "opel", 2500);
         Car yellowMazda = new Car("yellow", "mazda", 3000);
