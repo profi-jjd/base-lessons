5c0747cc420f12b2be33a12279ba65f99224ccbf
aud203 <ifmo@gmail.com>
2021-04-14 18:52:07 +0300

    collections

---
+++ src/com/profi/jjd/lesson13/CollectionApp.java
@@ -0,0 +1,10 @@
+package com.profi.jjd.lesson13;
+
+public class CollectionApp {
+    public static void main(String[] args) {
+        Student student1 = new Student("Петр", "Алексеев", 29);
+        Student student2 = new Student("Екатерина", "Еремина", 49);
+        Student student3 = new Student("Олег", "Попов", 54);
+        Student student4 = new Student("Алексей", "Попов", 54);
+    }
+}
