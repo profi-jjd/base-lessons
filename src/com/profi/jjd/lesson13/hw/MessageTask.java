f89ac317285d7d26fc014ffc21309728e4aaa5ff
frgyvu <my.email@gmail.com>
2021-04-14 13:51:42 +0300

    collections: tasks

---
+++ src/com/profi/jjd/lesson13/hw/MessageTask.java
@@ -0,0 +1,45 @@
+package com.profi.jjd.lesson13.hw;
+
+import java.util.*;
+
+public class MessageTask {
+    public static void countEachPriority(List<Message> messageList) {
+        // TODO:  Подсчитать количество сообщений для каждого приоритела
+        //  Ответ в консоль
+    }
+
+    public static void countEachCode(List<Message> messageList) {
+        // TODO: Подсчитать количество сообщений для каждого кода сообщения
+        //  Ответ в консоль
+    }
+
+    public static void uniqueMessageCount(List<Message> messageList) {
+        // TODO: Подсчитать количество уникальных сообщений
+        //  Ответ в консоль
+    }
+
+    public static List<Message> uniqueMessagesInOriginalOrder(List<Message> messageList) {
+        // TODO: вернуть только неповторяющиеся сообщения и в том порядке,
+        //  в котором они встретились в первоначальном списке
+        //  Например, было: [{URGENT, 4}, {HIGH, 9}, {LOW, 3}, {HIGH, 9}]
+        //  на выходе: [{URGENT, 4}, {HIGH, 9}, {LOW, 3}]
+        return null;
+    }
+
+    public static void removeEach(List<Message> messageList, Message.MessagePriority priority) {
+        // TODO: удалить из коллекции каждое сообщение с заданным приоритетом
+        //  вывод в консоль до удаления и после удаления
+
+    }
+
+    public static void removeOther(List<Message> messageList, Message.MessagePriority priority) {
+        // TODO: удалить из коллекции все сообщения, кроме тех, которые имеют заданный приоритет
+        //  вывод в консоль до удаления и после удаления
+    }
+
+    public static void main(String[] args) {
+        List<Message> messages = Message.MessageGenerator.generate(34);
+        System.out.println(messages);
+        // вызов методов
+    }
+}
---
+++ src/com/profi/jjd/lesson13/hw/compare/ComparingExample.java
@@ -0,0 +1,12 @@
+package com.profi.jjd.lesson13.hw.compare;
+
+public class ComparingExample {
+    public static void main(String[] args) {
+
+        // сортировать по значениям свойств: price, color, brand
+        Car blackOpel = new Car("black", "opel", 2000);
+        Car redOpel = new Car("red", "opel", 2500);
+        Car yellowMazda = new Car("yellow", "mazda", 3000);
+        Car greenMazda = new Car("green", "mazda", 3000);
+    }
+}
