485b6b8830cc2fbae5c562bfb9a1de939da78e20
frgyvu <my.email@gmail.com>
2021-04-14 14:21:31 +0300

    collections: tasks

---
+++ src/com/profi/jjd/lesson13/hw/EmployeeTask.java
@@ -0,0 +1,9 @@
+package com.profi.jjd.lesson13.hw;
+
+import java.util.List;
+
+public class EmployeeTask {
+    public static void main(String[] args) {
+        List<Employee> employees = Employee.employeeGenerator(20);
+    }
+}
