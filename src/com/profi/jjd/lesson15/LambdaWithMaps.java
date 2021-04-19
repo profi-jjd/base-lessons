d25b3bf9b503496906ccd68876327acfd2654602
aud203 <ifmo@gmail.com>
2021-04-19 21:57:58 +0300

    lambda ++

--- src/com/profi/jjd/lesson15/LambdaWithMaps.java
+++ src/com/profi/jjd/lesson15/LambdaWithMaps.java
@@ -4,6 +4,7 @@ import com.profi.jjd.lesson15.education.Task;
 
 import java.util.HashMap;
 import java.util.UUID;
+import java.util.function.BiConsumer;
 
 public class LambdaWithMaps {
     public static void main(String[] args) {
@@ -23,6 +24,22 @@ public class LambdaWithMaps {
         tasksMap.put(task5.getId(), task5);
         tasksMap.put(task6.getId(), task6);
         tasksMap.put(task7.getId(), task7);
+
+        // перебор мапы - метод foreach
+        // принимает на вход BiConsumer void accept(T t, U u)
+        BiConsumer<UUID, Task> biConsumer = (key, value) ->
+                System.out.println("key=" + key + ", value = " + value);
+        tasksMap.forEach(biConsumer);
+
+        BiConsumer<UUID, Task> updateDate = (key, value) ->
+                value.setFinishTo(value.getFinishTo().plusDays(1));
+        tasksMap.forEach(updateDate);
+
+        // вывести имена всех участников
+        tasksMap.forEach((key, value)->
+                value.getParticipants().forEach(System.out::println));
+
+        // TODO:: разобрать методы merge, computeIfPresent, computeIfAbsent
 
     }
 }
--- src/com/profi/jjd/lesson15/education/University.java
+++ src/com/profi/jjd/lesson15/education/University.java
@@ -3,6 +3,7 @@ package com.profi.jjd.lesson15.education;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Predicate;
 
 public class University {
     private List<Course> courses = new ArrayList<>();
@@ -11,11 +12,19 @@ public class University {
         return courses.add(Objects.requireNonNull(course));
     }
     public boolean addCourses(List<Course> courses) {
-        return courses.addAll(Objects.requireNonNull(courses));
+        return this.courses.addAll(Objects.requireNonNull(courses));
     }
 
     public List<Course> getCourses() {
         return courses;
+    }
+
+    public List<Course> getFilteredCourses(Predicate<Course> condition) {
+        List<Course> result = new ArrayList<>();
+        for (Course course : courses) {
+            if (condition.test(course)) result.add(course);
+        }
+        return result;
     }
 
 
