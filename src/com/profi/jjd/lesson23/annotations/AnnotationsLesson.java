f2e47dfbb7ec70257e7f5b6d0b8698ff2f7fbd2f
aud203 <ifmo@gmail.com>
2021-05-12 21:45:02 +0300

    lesson 23++

--- src/com/profi/jjd/lesson23/annotations/AnnotationsLesson.java
+++ src/com/profi/jjd/lesson23/annotations/AnnotationsLesson.java
@@ -3,10 +3,13 @@ package com.profi.jjd.lesson23.annotations;
 import com.profi.jjd.lesson23.Point;
 import com.profi.jjd.lesson23.Text;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Properties;
 import java.util.Set;
 
 public class AnnotationsLesson {
@@ -33,15 +36,33 @@ public class AnnotationsLesson {
         // если класс аннотирован Component:
         // создать объект данного класса
         // если поле отмечено аннотацией Required,
-        // установить значение данного поля, используя сеттер!!!
+        // установить значение (из properties файла) данного поля,
+        // используя сеттер!!!
         // int x setX(int x)
         // String name setName(String name)
+
 
 
         /*Set<Class<?>> classSet = new HashSet<>();
         classSet.add(Point.class);
         classSet.add(Text.class);*/
 
+        // import java.util.Properties;
+        Properties properties = new Properties();
+        properties.setProperty("ip", "127.0.0.1");
+        properties.setProperty("port", "8090");
+        System.out.println(properties.getProperty("ip")); // "127.0.0.1"
+        System.out.println(properties.getProperty("port")); // "8090"
+
+        try (InputStream input = AnnotationsLesson.class
+                .getClassLoader()
+                .getResourceAsStream("point.properties")){
+            properties.load(input);
+        } catch (IOException e) {
+            System.out.println("Невозможно прочитать файл");
+        }
+        System.out.println(properties.get("x"));
+        System.out.println(properties.get("y"));
 
     }
 }
