ffea74ad4040dc8baa5829f0d8e7246922c3f346
aud203 <ifmo@gmail.com>
2021-05-19 20:05:32 +0300

    lesson 26

--- src/com/profi/jjd/multithreading/lesson26/sync/Synchronized.java
+++ src/com/profi/jjd/multithreading/lesson26/sync/Synchronized.java
@@ -1,6 +1,10 @@
 package com.profi.jjd.multithreading.lesson26.sync;
 
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 public class Synchronized {
     public static void main(String[] args) {
@@ -24,5 +28,22 @@ public class Synchronized {
 
         // th1 - account 10
         // th2 - account 10
+
+        // List<String>  Files.readAllLines(Path.of("путь к файлу"));
+        // читает из файла в список, где элемент списка - строа из файла
+
+        System.out.println(Runtime.getRuntime().availableProcessors());
+
+        // HashMap<String, Integer> words
+
+        String text = "It is a uncover long established fact that a reader will be distracted uncover by the readable content of a page " +
+                "when looking at its layout The point of using uncover Lorem Ipsum is that sites it has a more-or-less normal distribution of letters" +
+                "as uncover opposed to still using Content here humour uncover content here making it look like readable English Many desktop publishing " +
+                "packages and web page editors now use Lorem Ipsum as their default model text and a search for lorem ipsum will " +
+                "uncover many web sites still uncover in their infancy Various versions uncover have evolved over the years uncover sometimes by accident" +
+                " sometimes on purpose injected humour and the like";
+
+        // main: words
+
     }
 }
--- src/com/profi/jjd/multithreading/lesson26/task/Transaction.java
+++ src/com/profi/jjd/multithreading/lesson26/task/Transaction.java
@@ -1,8 +1,8 @@
 package com.profi.jjd.multithreading.lesson26.task;
 
 public class Transaction implements Runnable{
-    private Account src;
-    private Account dst;
+    private final Account src;
+    private final Account dst;
     private int money;
 
     public Transaction(Account src, Account dst, int money) {
@@ -11,9 +11,27 @@ public class Transaction implements Runnable{
         this.money = money;
     }
 
-
+// (account1, account2, 300)
+// (account2, account1, 340)
     @Override
     public void run() {
         // перевод с src на dst
+        /*if (src.getUuid() < dst.getUuid()) {
+            synchronized (src) {
+                synchronized (dst){
+
+                }
+            }
+        } else {
+            synchronized (dst) {
+                synchronized (src){
+
+                }
+            }
+        }*/
+        synchronized (src) {}
+        synchronized (dst) {}
+
+
     }
 }
